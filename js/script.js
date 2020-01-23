f14a7e24cbd951a6496d8334cc014f43a9d0c1b6
Marco <marco.carone@libero.it>
2020-01-23 21:34:08 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-document.ready(function() {
+$(document).ready(function() {
 
+  $(document).on('click', '.message_send', function() {
+    var testoInput = $('.input_message').val();
+    var messaggioClonato = $('.container-message + .template_me').clone();
+    messaggioClonato.find('.message_text').text(testoInput);
+    $('.container-message').append(messaggioClonato);
+    $('.container-message .template_me').removeClass('display-none');
+  });
 });
