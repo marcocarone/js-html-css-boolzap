04920cb5651c0a1416cb94252ef941ba8c6611ca
Marco <marco.carone@libero.it>
2020-01-23 22:18:17 +0100

    aggiornamento js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
 
   $(document).on('click', '.message_send', function() {
-    var testoInput = $('.input_message').val();
-    var messaggioClonato = $('.container-message + .template_me').clone();
-    messaggioClonato.find('.message_text').text(testoInput);
-    $('.container-message').append(messaggioClonato);
-    $('.container-message .template_me').removeClass('display-none');
+    invioMessaggio()
   });
+
+  $('.input_message').keypress(
+    function() {
+      if (event.which == 13 || event.keyCode == 13) {
+        invioMessaggio()
+      }
+    }
+  );
 });
+
+
+function invioMessaggio() {
+  var testoInput = $('.input_message').val();
+  var messaggioClonato = $('.container-message + .template_me').clone();
+  messaggioClonato.find('.message_text').text(testoInput);
+  $('.container-message').append(messaggioClonato);
+  $('.container-message .template_me').removeClass('display-none');
+  $('.input_message').val("");
+}
