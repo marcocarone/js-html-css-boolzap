f3285494d83dc839d201e1e0b096522be82e4ef9
Marco <marco.carone@libero.it>
2020-01-24 09:03:55 +0100

    ag. js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
 
   $(document).on('click', '.message_send', function() {
     invioMessaggio();
+    setTimeout(function() {
+      MessaggioRisposta();
+    }, 2000);
   });
-
 
   var visible = false;
   $(document).on('click', '.message__icon', function() {
-
     if (visible == false) {
       $(this).find('.message_menu').fadeIn();
       visible = true;
@@ -26,12 +27,9 @@ $(document).ready(function() {
   );
 
   $(document).on('click', '.delete-message', function() {
-
-      $this.find('#remove').remove();
-
+    $(this).parent().parent().parent().remove();
   });
 });
-
 
 function invioMessaggio() {
   var testoInput = $('.input_message').val();
@@ -40,4 +38,11 @@ function invioMessaggio() {
   $('.container-message').append(messaggioClonato);
   $('.container-message .template_me').removeClass('display-none');
   $('.input_message').val("");
+}
+
+function MessaggioRisposta() {
+  var messaggioRisposta2 = $('.template-user').children().clone();
+  $('.container-message').append(messaggioRisposta2);
+  $('.container-message .message__user').removeClass('display-none');
+
 }
